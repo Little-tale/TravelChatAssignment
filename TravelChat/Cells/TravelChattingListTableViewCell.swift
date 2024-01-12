64274be6bcdcec89a0b036efba84e436f8f2d477
judge_happily <you3192001@gmail.com>
2024-01-12 20:40:11 +0900

    첫번째뷰 베이스 정도

---
+++ TravelChat/Cells/TravelChattingListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelChattingListTableViewCell.swift
+//  TravelChat
+//
+//  Created by Jae hyung Kim on 1/12/24.
+//
+
+import UIKit
+
+class TravelChattingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TravelChat/TravelChattingList/TravelChattingListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelChattingListViewController.swift
+//  TravelChat
+//
+//  Created by Jae hyung Kim on 1/12/24.
+//
+
+import UIKit
+
+class TravelChattingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
